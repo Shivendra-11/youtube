7db47c00edbc6061b4b86202c6fe08cdf56be23a
shivendra <shivendrakeshari11@gmail.com>
2024-04-30 21:42:18 +0530

    update

--- src/components/Livechat.js
+++ src/components/Livechat.js
@@ -1,40 +1,53 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Chatmessage from './Chatmessage'
 import {useDispatch, useSelector} from "react-redux"
 import { addMessage } from '../utils/Chatslice';
+import { generateRandomName, makeRandomChat } from '../utils/Helper';
+ 
 
 
 const Livechat = () => {
+  const [LiveMessage,setLiveMessage]=useState("");
     const dispatch=useDispatch();
     const Chats=useSelector((state)=>state.chat.messages);
     useEffect(()=>{
        const i = setInterval(() => {
             // console.log("hello")
             dispatch(addMessage({
-           name:"shivendra ",
-           message:"hello jee kya haal chal bdiya ekdum 😂",     
+           name:generateRandomName(),
+           message:makeRandomChat(25) ,     
             }));
         }, 2000);
         return ()=>clearInterval(i);
     
     },[]);
   return (
-    
-    <div className= 'ml-2  flex-col p-2 bg-slate-100  rounded-lg border-black border   h-[600px]  ' >
+    <>
+    <div className= 'ml-2   p-2 bg-slate-100 flex overflow-y-scroll flex-col-reverse rounded-lg border-black border   h-[600px]  ' >
         {/* Dont use indexes as a key */}
 
 {
   Chats.map((c,i)=>  <Chatmessage  key={i} name={c.name} message={c.message}  />)
 }
-        
-      
-        
-
-      
     
-
     </div>
-    
+    <form className='w-full p-2 ml-2 border border-black '
+     onSubmit={(e)=>{
+      e.preventDefault();
+      //  console.log("hello jee")
+      dispatch(
+        addMessage({
+        name:"Shivendra keshari",
+        message:LiveMessage,
+      }));
+      setLiveMessage("");
+      }}
+       
+       >
+      <input className='w-[400px] p-2 '  type="text"  value={LiveMessage} onChange={(e)=>setLiveMessage(e.target.value)}  />
+      <button className='px-2 mx-2 bg-green-500' >Send</button>
+    </form>
+    </>
   )
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export  const GOOGLE_API_KEY = "AIzaSyDYDj8HiIJtcwepyJBESow6i0F_-qoWex0";
 
+export const LIVE_CHAT_COUNT=20;
+
 export const YOUTUBE_POPULAR_VIDEOS =
   " https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
